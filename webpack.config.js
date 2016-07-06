b2aaefa48361d9bc900f174ff1c58622707d38c0
Yan Zhabin <iyanello@gmail.com>
2016-07-06 20:30:16 +0300

    Added local webpack execution instead global. Updated packages version in package.json and updated webpack config. Updated README.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-var path = require('path'),
-    webpack = require('webpack'),
-    extractTextPlugin = require('extract-text-webpack-plugin'),
-    root = __dirname,
+// let path = require('path'),     webpack = require('webpack'),     extractTextPlugin =
+// require('extract-text-webpack-plugin'),     root = __dirname,     config = {},     extractTextPlugin = new
+// extractTextPlugin('assets/style.css'),     env = process.env.NODE_ENV         ? process.env.NODE_ENV.trim()         :
+// 'production';
+
+let path = require('path');
+
+let webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let env = global.process.env.NODE_ENV ? global.process.env.NODE_ENV.trim() : 'production',
     config = {},
-    extractTextPlugin = new extractTextPlugin('assets/style.css'),
-    env = process.env.NODE_ENV ? process.env.NODE_ENV.trim() : 'production';
+    plugins = [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            ENV: JSON.stringify(env), // 'development' | 'test' | 'production'
+        }),
+        new webpack.ProvidePlugin({}),
+        new ExtractTextPlugin('assets/css/style.css')
+    ];
 
 if (env == 'production') {
     config = require('./webpack.production.js');
@@ -14,48 +29,41 @@ if (env == 'production') {
     config = require('./webpack.test.js');
 }
 
-var plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.CommonsChunkPlugin('commons.js'),
-    new webpack.DefinePlugin({
-        ENV: JSON.stringify(env), // 'development' | 'test' | 'production'
-    }),
-    new webpack.ProvidePlugin({}),
-    extractTextPlugin
-];
-
-if (config.uglify) plugins.push(new webpack.optimize.UglifyJsPlugin({
-    output: {
-        comments: false
-    }
-}));
+if (config.uglify) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        output: {
+            comments: false
+        }
+    }));
+}
 
 module.exports = {
-    context: path.resolve(root, 'app'),
+    context: path.resolve(__dirname, 'app'),
     devtool: config.devtool,
-    extensions: ['', '.js', '.styl', '.css'],
+    extensions: [
+        '', '.js', '.styl', '.css'
+    ],
     entry: {
-        app: path.resolve(root, 'app/js/main.js'),
+        app: path.resolve(__dirname, 'app/js/main.js'),
         // test: path.resolve(root, 'app/js/libs/test.js')
     },
     output: {
-        path: path.resolve(root, 'public'),
-        libraryTarget: 'umd',
-        filename: '[name].bundle.js',
-        publicPath: '/assets/',
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/assets/'
     },
     resolve: {
         alias: {
-            css: path.resolve(root, 'app/stylus'),
-            libs: path.resolve(root, 'app/js/libs'),
-            templates: path.resolve(root, 'app/templates')
-        },
+            css: path.resolve(__dirname, 'app/stylus'),
+            libs: path.resolve(__dirname, 'app/js/libs'),
+            templates: path.resolve(__dirname, 'app/templates')
+        }
     },
     module: {
         loaders: [{
-            test: [/\.styl$/, /\.css$/],
-            loader: extractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
+            test: [
+                /\.styl$/, /\.css$/
+            ],
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
         }, {
             test: /\.(woff|svg|ttf|eot)([\?]?.*)$/,
             loader: "file-loader?name=[name].[ext]"
@@ -66,9 +74,6 @@ module.exports = {
         contentBase: path.resolve(root, 'public')
     },
     proxy: {
-        // '/some/path*': {
-        //     target: 'https://other-server.example.com',
-        //     secure: false,
-        // },
+        // '/some/path*': {     target: 'https://other-server.example.com',     secure: false, },
     }
 };
